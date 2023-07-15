0432ca5e31b7cca3b9d5df8d204e3800d7aa2c9c
Robson Coutinho <robson.coutinho@am4.com.br>
2023-07-15 08:52:28 -0300

    adicionado Unit of work

---
+++ src/DotnetJetpack.Core/Contracts/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetJetpack.Core.Contracts.Infrastructure;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync();
+    Task BeginTransactionAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
+}
---
+++ src/DotnetJetpack.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using DotnetJetpack.Core.Contracts.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetJetpack.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.Database.CommitTransactionAsync();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _dbContext.Database.RollbackTransactionAsync();
+        }
+    }
+}
