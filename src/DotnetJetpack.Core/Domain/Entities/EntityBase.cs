49f1a656af718cc003e436464d60579f8298aac5
Robson Coutinho <robson.coutinho@am4.com.br>
2023-07-17 13:51:46 -0300

    adicionado EntityBase.cs

---
+++ src/DotnetJetpack.Core/Domain/Entities/EntityBase.cs
@@ -0,0 +1,29 @@
+﻿namespace DotnetJetpack.Core.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; protected set; }
+
+        public virtual Notification Notification { get; protected set; } = new Notification();
+
+        public void AddError(string error)
+        {
+            Notification.AddError(error);
+        }
+
+        public void AddErrors(Notification notification)
+        {
+            Notification.AddErrors(notification.Errors);
+        }
+
+        public void ClearErrors()
+        {
+            Notification.ClearErrors();
+        }
+
+        public bool HasErrors()
+        {
+            return Notification.HasErrors;
+        }
+    }
+}
