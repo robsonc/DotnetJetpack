e5b3fbdee0ddac743d43489633ed72487afa8b7c
Robson Coutinho <robson.coutinho@am4.com.br>
2023-07-17 13:51:20 -0300

    adicionado notification

---
+++ src/DotnetJetpack.Core/Domain/Notification.cs
@@ -0,0 +1,26 @@
+﻿namespace DotnetJetpack.Core.Domain
+{
+    public class Notification
+    {
+        private readonly List<string> _errors = new List<string>();
+
+        public IReadOnlyCollection<string> Errors => _errors;
+
+        public bool HasErrors => _errors.Count > 0;
+
+        public void AddError(string error)
+        {
+            _errors.Add(error);
+        }
+
+        public void AddErrors(IEnumerable<string> errors)
+        {
+            _errors.AddRange(errors);
+        }
+
+        public void ClearErrors()
+        {
+            _errors.Clear();
+        }
+    }
+}
